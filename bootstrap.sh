510e88dd95feea4d778ac97a4243b6b2b3367cbc
CiaraTully <ciara.tully@runa.io>
2023-10-11 16:44:56 +0100

    Add a bootstrap script
    Why?
    To setup new mac
    
    How?
    Update homebrew
    
    Install GUI apps using cask
    
    Install commomn brew packages
    
    This script can be configured to only be run on intel 64-bit architecture
    
    This script is heavily inspired by https://github.com/morleyzhi/bootstrap.sh/blob/master/bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Bootstrap script for setting up a new OSX machine using x86_64 architecture. 
+
+xcode-select -p || exit "XCode must be installed! (use the app store)"
+
+# Uncomment if you want to only allow the script to run on x86_64 architecture	
+#arch=$(uname -m)
+#permitted_architecture="x86_64"
+
+#if [ "$arch" != "$permitted_architecture" ]; then
+#	echo "In order to install these applications use intel 64bit architecture"
+#	echo "If you are on an M1/M2 mac, you can switch using the following command:"
+#	echo "\$env /usr/bin/arch -x86_64 /bin/zsh --login"
+#	echo "..exiting.."
+#	exit 1
+#fi
+
+
+# homebrew
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+if hash brew &>/dev/null; then
+	echo "Homebrew already installed. Getting updates..."
+	brew update
+	brew doctor
+else
+	echo "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+
+#install brew packages
+echo "installing packages"
+PACKAGES=(
+	cask
+	wget
+	zsh
+	olets/tap/zsh-abbr
+	neovim
+)
+brew install "${PACKAGES[@]}"
+
+
+#list of casks to install
+CASKS=(
+	iterm2
+	rectangle
+	raycast
+	visual-studio-code
+	)
+
+#install casks
+echo "installing casks"
+brew install --cask "${CASKS[@]}"
+
+
+
+
+
